7416575f17a17a01491c488c6b59422c048a54aa
Kim Wong <kimwong@kim-imac.local>
2019-02-22 19:49:49 +0800

    Initial Commit

---
+++ Xcode-test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Xcode-test
+//
+//  Created by Kim Wong on 22/2/2019.
+//  Copyright © 2019 Kim Wong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
